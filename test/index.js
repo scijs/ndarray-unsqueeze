94990c20a8e64da29da15df316c0330817be6467
Ricky Reusser <rsreusser@gmail.com>
2016-01-04 07:25:29 -0500

    Fix deps

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
 
 var ndarray = require('ndarray')
 var assert = require('chai').assert
-var unsqueeze = require('../unsqueeze')
+var unsqueeze = require('../')
 var ndt = require('ndarray-tests')
 
 describe('ndarray-unsqueeze', function () {
