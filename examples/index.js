304556f47b27693b4eb8bb8beaf946d84691b1b8
Ricky Reusser <rsreusser@gmail.com>
2016-01-04 07:32:55 -0500

    Cleanup

--- examples/index.js
+++ examples/index.js
@@ -2,5 +2,5 @@ var ndarray = require('ndarray')
 var unsqueeze = require('../')
 var show = require('ndarray-show')
 
-console.log(show(unsqueeze(ndarray([1,2,3]))))
-console.log(show(unsqueeze(ndarray([1,2,3]), 0)))
+console.log(show(unsqueeze(ndarray([1, 2, 3]))))
+console.log(show(unsqueeze(ndarray([1, 2, 3]), 0)))
